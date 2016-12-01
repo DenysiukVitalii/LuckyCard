34e978be06e899ff595e2da10085004fd07d5111
vitalii_denysiuk <dendoc97@gmail.com>
2016-12-02 00:02:22 +0200

    header and menu

---
+++ static/js/game.js
@@ -0,0 +1,13 @@
+var touch = $('#mobmenu');
+var menu = $('#menu');
+
+$(touch).on('click', function (e) {
+  e.preventDefault();
+  menu.slideToggle();
+});
+$(window).resize(function () {
+  var wid = $(window).width();
+  if (wid > 760 && menu.is(':hidden')) {
+    menu.removeAttr('style');
+  }
+});
