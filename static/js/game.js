99f6d670f1ef749dec6eb73c943e9ed68462d17a
vitalii_denysiuk <dendoc97@gmail.com>
2016-12-18 22:55:32 +0200

    add section tabs, add timer

--- static/js/game.js
+++ static/js/game.js
@@ -11,3 +11,27 @@ $(window).resize(function () {
     menu.removeAttr('style');
   }
 });
+
+$('.tabs li').on('click', function () {
+    $('.tabs').height( $('.tab-content', $(this) ).height() + 30);
+});
+
+$('.bet-amount').on('click', function () {
+  $('#exampleInputAmount').val($(this).text() + ".00");
+});
+
+
+
+var counter = 21;
+function Timer () {
+      counter--;
+      if (counter > 9) document.getElementById('timer').innerHTML = "0:" + counter;
+      else if (counter <= 9) document.getElementById('timer').innerHTML = "0:0" + counter;
+      if (counter > 0) setTimeout(Timer, 1000);
+      else {
+        counter = 21;
+        setTimeout(Timer, 3000);
+      }
+}
+
+Timer();
